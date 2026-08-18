279e4a975fa8240547420f9260e62e7af616e7d9
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-08-18 12:15:53 -0300

    Add- Estrutura tela pratos

---
+++ pratos.php
@@ -0,0 +1,18 @@
+<?php
+
+include "../infra/conexao.php";
+$pratos = mysqli_query($conexao, "SELECT * FROM pratos");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Pratos</title>
+</head>
+<body>
+    
+</body>
+</html>
