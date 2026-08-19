067ea7c197c9edfd7a286e89c82c00d4a97df343
nicole <nicole_b_silva@estudante.sesisenai.org.br>
2026-08-19 12:40:47 -0300

    Add-  excluir.php

--- public/excluir.php
+++ public/excluir.php
@@ -1,7 +1,23 @@
 <?php
+
 include "../infra/conexao.php";
-$id = $_GET["id"];
-$sql = "DELETE FROM livros WHERE id=$id";
-mysqli_query($conexao,$sql);
-header("Location: ../index.php");
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM pratos WHERE id = ?";
+
+$stmt = mysqli_prepare($conexao, $sql);
+
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+if (mysqli_stmt_execute($stmt)) {
+    header("Location: listar_pratos.php");
+    exit;
+} else {
+    echo "Erro ao excluir o prato.";
+}
+
+mysqli_stmt_close($stmt);
+mysqli_close($conexao);
+
 ?>
